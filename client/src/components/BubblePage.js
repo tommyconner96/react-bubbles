f626891b907efd2c33d95cb98a8025d01b6fd101
unknown <tommyconner96@gmail.com>
2020-05-15 19:58:05 -0400

    login page kinda built

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -9,6 +9,8 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  // useEffect geos here. axios get request for color data for bubbles
+
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
